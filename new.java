ec4cad6ff2e1a224014de5646041fe196f5a83cf
simonpok <poksimon9@gmail.com>
2023-02-13 20:35:12 +0545

    initial commit

---
+++ new.java
@@ -0,0 +1,13 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello My name is simon");
+      System.out.println("Level= Bachelor");
+      System.out.println("sem = 4th");
+      System.out.println("Faculty= BIT");
+
+   }
+
+}
+
---
+++ simon.java
@@ -0,0 +1,18 @@
+public class Business
+{
+   public static void main (String[] args)
+   {
+   
+   System.out.println("Name of the Organization: Ramlal paan pasal");
+   System.out.println("\nAddress:");
+   System.out.println("----State: Butwal");
+   System.out.println("----Zip Code: 32900");
+   System.out.println("----Country: Nepal");
+   System.out.println("\nContact Information:");
+   System.out.println("----Mobile Number: 9876543210");
+   System.out.println("----Email: paanpasal@gmail.com");
+   }
+}
+
+
+
