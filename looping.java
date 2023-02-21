a8e9fdd7b2345faba7299cf86176c98538c0abd9
poksimon9@gmail.com <poksimon9@gmail.com>
2023-02-21 21:58:15 +0545

    update

---
+++ looping.java
@@ -0,0 +1,22 @@
+public class Main{
+
+
+   public static void main(String[] args)
+   {
+   /*int money=1;
+      for(int i=1;i<=10;i++)
+       {
+          money = money*i;
+          System.out.println(money);
+          }*/
+       int money =1;   
+      while (int i <=10)
+      {
+       money *= i;
+       i++;
+      }
+      System.out.println(money);
+      
+   }
+   }
+      
---
+++ sprgame.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main{
+
+
+   public static void main(String[] args)
+   {
+   boolean flag=true;
+   do {
+   //int i=0;
+   //while (i=0){
+     // take user input
+     //S,P,R
+     Scanner inputObj = new Scanner(System.in);
+     System.out.println("Please Enter S,P,R");
+     char userMove = inputObj.next().charAt(0);  
+     System.out.println("User pressed>>>>"+userMove);
+     
+    char[] maxMove ={'S', 'P', 'R'};
+    Random random = new Random();
+    //0-2
+    
+    int number = random.nextInt(3);
+    char computerMove =maxMove[number];
+    System.out.println("Computer move>>"+computerMove);
+    
+    
+    
+     
+     //make computer to make move either S,P,R
+     //computer =user -> DRAW
+     //Computer "S"
+       //user "P" -> computer wins.
+       //user Wins.
+       
+     //Computer "P"
+     //User "S" -> user  wins
+     //computer wins.
+      if(userMove =='S' && computerMove =='S' )
+   {
+      System.out.println("tie!!");
+   }
+  else if(userMove =='S' && computerMove =='R' )
+   {
+      System.out.println("you lose");
+   }
+   else if(userMove =='S' && computerMove =='P' )
+   {
+      System.out.println("You win");
+   }
+   else if(userMove =='P' && computerMove =='S' )
+   {
+      System.out.println("You lose!!");
+   }
+   else if(userMove =='P' && computerMove =='R' )
+   {
+      System.out.println("you win");
+   }
+   else if(userMove =='P' && computerMove =='P' )
+   {
+      System.out.println("tie!!");
+   }
+   else if(userMove =='R' && computerMove =='S' )
+   {
+      System.out.println("You win!!");
+   }
+   else if(userMove =='R' && computerMove =='R' )
+   {
+      System.out.println("tie!!");
+   }
+   else if(userMove =='R' && computerMove =='P' )
+   {
+      System.out.println("You lose");
+   }
+   else if(userMove =='R' && computerMove =='P' )
+   {
+      System.out.println("You lose");
+   }
+   else if
+   {
+      System.out.println("Type the valid input, please!");
+   }
+   
+  }while(flag=true);
+  
+  }
+   
+} 
